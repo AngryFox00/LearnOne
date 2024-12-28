b776c203c93596ddbfd5b86f773dc0b18c781ac5
AngryFox00 <yelyzavetamieshkova00@gmail.com>
2024-12-28 21:11:15 +0100

    Add second component

---
+++ learn_2/src/components/DimaComponent.jsx
@@ -0,0 +1,10 @@
+function DimaComponent() {
+  return (
+    <div>
+      <h1>Dima is my pretty kitty!</h1>
+      <button>No!</button>
+    </div>
+  );
+}
+
+export default DimaComponent;
---
+++ learn_2/src/components/MyComponents.jsx
@@ -0,0 +1,11 @@
+
+function MyComponent() {
+    return (
+        <div>
+            <h1> Hello Dima </h1>
+            <button>Love!</button>
+        </div>
+    );
+}
+
+export default MyComponent
